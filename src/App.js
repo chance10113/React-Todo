db723670527d1b79359308f2a19ada6037940fe7
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-02-08 17:10:31 -0600

    tentatively completed App and ToDoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ToDoList from "./components/TodoList";
+import ToDoForm from "./components/TodoForm";
+const toDos = [
+  {
+    task: "Overcome Anxiety",
+    id: 7,
+    completed: false,
+  },
+  {
+    task: "Survive Today",
+    id: 14042,
+    completed: false,
+  },
+  {
+    task: "Love Yourself More",
+    id: 5,
+    completed: false,
+  },
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      toDos: toDos,
+    };
+  }
+
+  // Change Handlers
+
+  // Updates State
+  crossOff = (clickedToDoId) => {
+    const newToDos = this.state.toDos.map((toDo) => {
+      if (toDo.id === clickedToDoId) {
+        return {
+          ...toDo,
+          completed: !toDo.completed,
+        };
+      } else {
+        return toDo;
+      }
+    });
+    this.setState({
+      toDos: newToDos,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1>ToDo List:</h1>
+          <ToDoForm />
+        </div>
+        <ToDoList toDos={this.state.toDos} crossOff={this.state.crossOff} />
       </div>
     );
   }
